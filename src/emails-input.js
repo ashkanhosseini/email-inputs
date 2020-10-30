c856e5084b6620e9b532475b8c20457e0b7899c5
Ashkan <ashkan.hosseini@gmail.com>
2020-10-30 23:21:11 +0100

    changed to inner html for IE11 compatibility

--- src/emails-input.js
+++ src/emails-input.js
@@ -19,25 +19,29 @@ const getTextInput = () => {
 const getContainer = () => {
   const container = document.createElement('div');
   container.classList.add('emails-input');
+  container.state = { emails: [] };
   return container;
 };
 
 function EmailsInput(node, options) {
   const container = getContainer();
-  const input = getTextInput();
-  let emails = [];
+  // const input = getTextInput();
+  // let emails = [];
   if (!isKeypressAttached) {
     document.addEventListener('keypress', (event) => {
       if (
-        event.target === input &&
+        event.target.classList.contains('emails-input__input') &&
         (event.charCode === 44 || event.charCode === 13)
       ) {
+        const input = event.target;
         event.preventDefault();
+        const { emails } = container.state;
         const status = isEmailValid(input.value) ? 'valid' : 'invalid';
         emails.push({ value: input.value, status });
         input.value = '';
         render();
-        container.dataset['state'] = emails;
+        // container.dataset['state'] = emails;
+        // container.insertBefore()
         input.focus();
       }
     });
@@ -45,19 +49,37 @@ function EmailsInput(node, options) {
   }
 
   const render = () => {
-    const elements = emails.reduce((acc, email) => {
-      const el = document.createElement('div');
-      el.classList.add('emails-input__tag');
-      el.innerText = email.value;
-      if (email.status !== 'valid') {
-        el.classList.add('emails-input__tag--invalid');
-      }
-      acc.push(el);
-      return acc;
-    }, []);
-    elements.push(input);
-    container.replaceChildren(...elements);
-    node.replaceChildren(container);
+    console.log({ emails: container.state.emails });
+    let emailsTmpl = container.state.emails.reduce((tmpl, email) => {
+      tmpl += `
+        <div class="emails-input__tag ${
+          email.status === 'invalid' ? 'emails-input__tag--invalid' : ''
+        }">
+          ${email.value}
+        </div>
+      `;
+      return tmpl;
+    }, '');
+    emailsTmpl += `<input type="text" class="emails-input__input" placeholder="add more people..." />`;
+    console.log({ emailsTmpl });
+    container.innerHTML = emailsTmpl;
+    node.innerHTML = '';
+    node.appendChild(container);
+    const [input] = container.getElementsByClassName('emails-input__input');
+    input.focus();
+    // const elements = container.state.emails.reduce((acc, email) => {
+    //   const el = document.createElement('div');
+    //   el.classList.add('emails-input__tag');
+    //   el.innerText = email.value;
+    //   if (email.status !== 'valid') {
+    //     el.classList.add('emails-input__tag--invalid');
+    //   }
+    //   acc.push(el);
+    //   return acc;
+    // }, []);
+    // elements.push(input);
+    // container.replaceChildren(...elements);
+    // node.replaceChildren(container);
   };
 
   render();
