4f0d6e3c1bc4517148f72e3d8a622cb6cab53f0a
Ashkan <ashkan.hosseini@gmail.com>
2020-10-31 22:45:06 +0100

    Implemented paste

--- src/emails-input.js
+++ src/emails-input.js
@@ -6,15 +6,7 @@ const isEmailValid = (email) => {
   );
 };
 
-let isKeypressAttached = false;
-
-const getTextInput = () => {
-  const input = document.createElement('input');
-  input.setAttribute('type', 'text');
-  input.setAttribute('placeholder', 'add more people...');
-  input.classList.add('emails-input__input');
-  return input;
-};
+let areGlobalHandlersAttached = false;
 
 const getContainer = () => {
   const container = document.createElement('div');
@@ -33,7 +25,6 @@ const render = (container, root, focus = true) => {
   if (!root) {
     root = container.parentElement;
   }
-  console.log({ emails: container.state.emails });
   let emailsTmpl = container.state.emails.reduce((tmpl, email) => {
     tmpl += `
       <div class="emails-input__tag ${
@@ -48,26 +39,12 @@ const render = (container, root, focus = true) => {
   }, '');
   emailsTmpl += `<input type="text" class="emails-input__input" placeholder="add more people..." />`;
   container.innerHTML = emailsTmpl;
-  console.log({ container });
   root.innerHTML = '';
   root.appendChild(container);
   const [input] = container.getElementsByClassName('emails-input__input');
   if (focus) {
     input.focus();
   }
-  // const elements = container.state.emails.reduce((acc, email) => {
-  //   const el = document.createElement('div');
-  //   el.classList.add('emails-input__tag');
-  //   el.innerText = email.value;
-  //   if (email.status !== 'valid') {
-  //     el.classList.add('emails-input__tag--invalid');
-  //   }
-  //   acc.push(el);
-  //   return acc;
-  // }, []);
-  // elements.push(input);
-  // container.replaceChildren(...elements);
-  // node.replaceChildren(container);
 };
 
 const isEmailsInput = (element) =>
@@ -117,12 +94,10 @@ const handlePaste = (event) => {
 
 function EmailsInput(root, options) {
   const container = getContainer();
-  // const input = getTextInput();
-  // let emails = [];
-  if (!isKeypressAttached) {
+  if (!areGlobalHandlersAttached) {
     document.addEventListener('keypress', handleKeyPress);
     document.addEventListener('paste', handlePaste);
-    isKeypressAttached = true;
+    areGlobalHandlersAttached = true;
   }
 
   render(container, root, false);
