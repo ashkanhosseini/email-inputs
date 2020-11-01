ca2c62c973bf37a72d247594a88f5b5fa598473a
Ashkan <ashkan.hosseini@gmail.com>
2020-11-01 22:59:09 +0100

    Form styles

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 import './styles.scss';
 
 import EmailsInput from './emails-input';
-const input = EmailsInput(document.getElementById('root'), {
+const input = EmailsInput(document.getElementById('emails'), {
   onChange: (event, value) => console.log({ event, value })
 });
-console.log({ input });
+// console.log({ input });
 
-const test = document.getElementById('test');
-console.log(test);
-test.addEventListener('click', () =>
-  input.replaceAll(['asdd', 'wqe@add.com', 'wqe@ss', '45423@ua.com'])
-);
+// const test = document.getElementById('test');
+// console.log(test);
+// test.addEventListener('click', () =>
+//   input.replaceAll(['asdd', 'wqe@add.com', 'wqe@ss', '45423@ua.com'])
+// );
 // test.onclcik = () => {
 //   console.log('asdasdasdasd');
 // };
