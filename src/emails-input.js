e657f0ab0cad763e938d9b969ffcd19e58ac7ccf
Ashkan <ashkan.hosseini@gmail.com>
2020-11-01 12:08:15 +0100

    Refactored to prevent redundant renders after each email

--- src/emails-input.js
+++ src/emails-input.js
@@ -8,7 +8,7 @@ const isEmailValid = (email) => {
 
 let areGlobalHandlersAttached = false;
 
-const getContainer = () => {
+const initContainer = (root) => {
   const container = document.createElement('div');
   container.classList.add('emails-input');
   container.state = { emails: [] };
@@ -18,37 +18,39 @@ const getContainer = () => {
       target.parentElement.parentElement.removeChild(target.parentElement);
     }
   });
+  container.innerHTML = `<input type="text" class="emails-input__input" placeholder="add more people..." />`;
+  root.appendChild(container);
   return container;
 };
 
-const render = (container, root, focus = true) => {
-  if (!root) {
-    root = container.parentElement;
-  }
-  let emailsTmpl = container.state.emails.reduce((tmpl, email) => {
-    tmpl += `
-      <div class="emails-input__tag ${
-        email.status === 'invalid' ? 'emails-input__tag--invalid' : ''
-      }">
-      
-        <span>${email.value}</span>
-        <span class="emails-input__remove-tag">x</span>
-      </div>
+const render = ({ container, input }, newEmails) => {
+  container.state.emails = container.state.emails.concat(newEmails);
+  newEmails.forEach((email) => {
+    const emailContainer = document.createElement('div');
+    emailContainer.classList.add(
+      'emails-input__tag',
+      `emails-input__tag--${email.status}`
+    );
+    emailContainer.innerHTML = `
+      <span>${email.value}</span>
+      <span class="emails-input__remove-tag">x</span>
     `;
-    return tmpl;
-  }, '');
-  emailsTmpl += `<input type="text" class="emails-input__input" placeholder="add more people..." />`;
-  container.innerHTML = emailsTmpl;
-  root.innerHTML = '';
-  root.appendChild(container);
-  const [input] = container.getElementsByClassName('emails-input__input');
-  if (focus) {
-    input.focus();
-  }
+    container.insertBefore(emailContainer, input);
+  });
+
+  // emailsTmpl += `<input type="text" class="emails-input__input" placeholder="add more people..." />`;
+  // container.innerHTML = emailsTmpl;
+  // root.innerHTML = '';
+  // root.appendChild(container);
+  // const [input] = container.getElementsByClassName('emails-input__input');
+  // if (focus) {
+  //   input.focus();
+  // }
 };
 
 const isEmailsInput = (element) =>
   element.classList.contains('emails-input__input');
+
 const getEmailObj = (value) => {
   const status = isEmailValid(value) ? 'valid' : 'invalid';
   return { value, status };
@@ -59,14 +61,14 @@ const handleKeyPress = (event) => {
     isEmailsInput(event.target) &&
     (event.charCode === 44 || event.charCode === 13)
   ) {
+    event.preventDefault();
     const input = event.target;
     const container = input.parentElement;
-    event.preventDefault();
-    const { emails } = container.state;
-    emails.push(getEmailObj(input.value));
+    // const { emails } = container.state;
+    // emails.push(getEmailObj(input.value));
+    render({ container, input }, [getEmailObj(input.value)]);
     input.value = '';
-    render(container);
-    input.focus();
+    // input.focus();
   }
 };
 
@@ -76,31 +78,24 @@ const handlePaste = (event) => {
     const container = input.parentElement;
     let paste = (event.clipboardData || window.clipboardData).getData('text');
     if (paste) {
-      console.log({
-        paste,
-        container,
-        res: container.state.emails.concat(
-          paste.split(',').map((val) => val.trim())
-        )
-      });
-      container.state.emails = container.state.emails.concat(
-        paste.split(',').map((val) => getEmailObj(val.trim()))
-      );
-      render(container);
       event.preventDefault();
+      const pastedEmails = paste
+        .split(',')
+        .map((val) => getEmailObj(val.trim()));
+      render({ container, input }, pastedEmails);
     }
   }
 };
 
 function EmailsInput(root, options) {
-  const container = getContainer();
+  initContainer(root);
   if (!areGlobalHandlersAttached) {
     document.addEventListener('keypress', handleKeyPress);
     document.addEventListener('paste', handlePaste);
     areGlobalHandlersAttached = true;
   }
 
-  render(container, root, false);
+  // render(container, root, false);
   return {
     replaceAll: (emails) => {},
     getEmails: () => {}
