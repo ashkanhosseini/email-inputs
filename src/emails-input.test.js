03d7a397eee0c0f4afb290233b867460d8d8d535
Ashkan <ashkan.hosseini@gmail.com>
2020-11-02 22:16:39 +0100

    added tests for the public API

--- src/emails-input.test.js
+++ src/emails-input.test.js
@@ -1,24 +1,18 @@
 import {
   getByLabelText,
   getByText,
-  getByTestId,
-  queryByTestId,
-  getByPlaceholderText,
-  // Tip: all queries are also exposed on an object
-  // called "queries" which you could import here as well
-  waitFor
+  queryByText,
+  getByPlaceholderText
 } from '@testing-library/dom';
-// adds special assertions like toHaveTextContent
 import '@testing-library/jest-dom/extend-expect';
 import userEvent from '@testing-library/user-event';
 
 import EmailsInput from './emails-input';
 
-const render = () => {
+const render = (options) => {
   const root = document.createElement('div');
-  // root.setAttribute('')
   document.body.append(root);
-  return { container: document.body, emailsInput: EmailsInput(root) };
+  return { container: document.body, emailsInput: EmailsInput(root, options) };
 };
 
 const keyStrokesThatCreateBlocks = [',', '{enter}'];
@@ -30,19 +24,17 @@ describe('EmailsInput', () => {
 
   keyStrokesThatCreateBlocks.forEach((key) => {
     it(`should create an email block when user hits "${key}"`, () => {
+      const emails = ['blahblah', 'validemail@gmail.com'];
       const { container } = render();
+
       const input = getByPlaceholderText(container, /add more people/i);
-      const invalidEmail = 'blahblah';
-      const validEmail = 'validemail@gmail.com';
       expect(input).toBeVisible();
 
-      userEvent.type(input, invalidEmail + key);
-      expect(input.value).toBe('');
-      expect(getByText(container, invalidEmail));
-
-      userEvent.type(input, validEmail + key);
-      expect(input.value).toBe('');
-      expect(getByText(container, validEmail));
+      emails.forEach((email) => {
+        userEvent.type(input, email + key);
+        expect(input.value).toBe('');
+        expect(getByText(container, email));
+      });
 
       expect(container.querySelectorAll('.emails-input__tag')).toHaveLength(2);
       expect(
@@ -57,19 +49,15 @@ describe('EmailsInput', () => {
   it(`should create an email block when input looses focus`, () => {
     const { container } = render();
     const input = getByPlaceholderText(container, /add more people/i);
-    const invalidEmail = 'blahblah';
-    const validEmail = 'validemail@gmail.com';
+    const emails = ['blahblah', 'validemail@gmail.com'];
     expect(input).toBeVisible();
 
-    userEvent.type(input, invalidEmail);
-    userEvent.tab();
-    expect(input.value).toBe('');
-    expect(getByText(container, invalidEmail));
-
-    userEvent.type(input, validEmail);
-    userEvent.tab();
-    expect(input.value).toBe('');
-    expect(getByText(container, validEmail));
+    emails.forEach((email) => {
+      userEvent.type(input, email);
+      userEvent.tab();
+      expect(input.value).toBe('');
+      expect(getByText(container, email));
+    });
 
     expect(container.querySelectorAll('.emails-input__tag')).toHaveLength(2);
     expect(
@@ -79,6 +67,19 @@ describe('EmailsInput', () => {
       container.querySelectorAll('.emails-input__tag--valid')
     ).toHaveLength(1);
   });
+
+  // ! need to investigate, probably a bug in user-events that doesn't take preventDefault into account
+  // it('should convert pasted text into email blocks', async () => {
+  //   const emails = ['valid@gmail.com', 'whatsup', 'amIvalid@somewhere.com'];
+  //   const textToPaste = emails.join(', ');
+
+  //   const { container, emailsInput } = render();
+  //   const input = getByPlaceholderText(container, /add more people/i);
+  //   userEvent.paste(input, textToPaste, {
+  //     clipboardData: { getData: () => textToPaste }
+  //   });
+  //   await waitFor(() => expect(input.value).toBe(''));
+  // });
 
   it('should be able to remove email blocks', () => {
     const { container, emailsInput } = render();
@@ -105,5 +106,66 @@ describe('EmailsInput', () => {
     expect(
       container.querySelectorAll('.emails-input__tag--valid')
     ).toHaveLength(0);
+  });
+
+  describe('public API', () => {
+    it('#onChange', () => {
+      const onChange = jest.fn();
+      const { container } = render({ onChange });
+      const input = getByPlaceholderText(container, /add more people/i);
+
+      userEvent.type(input, 'ashkan@gmail.com{enter}');
+      expect(onChange).toHaveBeenCalledWith(
+        expect.objectContaining({ type: 'emailsInput.change' }),
+        [{ status: 'valid', value: 'ashkan@gmail.com' }]
+      );
+
+      userEvent.type(input, 'invalid-email{enter}');
+      expect(onChange).toHaveBeenCalledWith(
+        expect.objectContaining({ type: 'emailsInput.change' }),
+        [
+          { status: 'valid', value: 'ashkan@gmail.com' },
+          { status: 'invalid', value: 'invalid-email' }
+        ]
+      );
+      expect(onChange).toHaveBeenCalledTimes(2);
+    });
+
+    it('#replaceAll', () => {
+      const { container, emailsInput } = render();
+      const emails = ['valid@gmail.com', 'whatsup', 'amIvalid@somewhere.com'];
+      const replaceEmails = ['starwars@cool.com', 'or', 'startrek@cool.com'];
+      emailsInput.addEmails(emails);
+      emails.forEach((email) => {
+        expect(getByText(container, email)).toBeVisible();
+      });
+
+      emailsInput.replaceAll(replaceEmails);
+      emails.forEach((email) => {
+        expect(queryByText(container, email)).toBeNull();
+      });
+      replaceEmails.forEach((email) => {
+        expect(getByText(container, email)).toBeVisible();
+      });
+    });
+
+    it('#getState', () => {
+      const { container, emailsInput } = render();
+      const emails = ['pineapple@pizze.com', 'or', 'thisIsNotaPizza'];
+      const input = getByPlaceholderText(container, /add more people/i);
+
+      emails.forEach((email) => {
+        userEvent.type(input, email);
+        userEvent.tab();
+        expect(input.value).toBe('');
+        expect(getByText(container, email));
+      });
+
+      expect(emailsInput.getState()).toEqual([
+        { value: 'pineapple@pizze.com', status: 'valid' },
+        { value: 'or', status: 'invalid' },
+        { value: 'thisIsNotaPizza', status: 'invalid' }
+      ]);
+    });
   });
 });
