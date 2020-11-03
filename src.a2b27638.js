f79c4cb856b9d57b87793290690d5640f34cad30
Ashkan Hosseini <>
2020-11-03 21:52:58 +0100

    fixed issue with removing and count

--- src.a2b27638.js
+++ src.a2b27638.js
@@ -414,7 +414,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "63738" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50746" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
